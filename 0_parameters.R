8ddc532bfaffa8d94d1287dc099bce95bc427f9f
Vojtech Krejcirik <vojtakk@gmail.com>
2014-06-11 22:25:13 +0900

    declaration of all input parameters moved into 0_ script

---
+++ 0_parameters.R
@@ -0,0 +1,15 @@
+# setting constants and everything
+
+earthquakes_file_path <- "data/quakes2.csv"
+
+coastline_file_path <- "data/coastline2.dat"
+
+south_boundary<-0
+north_boundary<-20
+  
+west_boundary<-110
+east_boundary<-130
+
+distance<-0.5
+
+grid_spacing<-0.25
---
+++ data_modification.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "modify time variables in quake.csv to have the POSIXct format"
+
+FILE_path=data/quakes2.csv
+
+#T character between date and time
+sed -i 's/\([0-9][0-9]\)T/\1 /g' $FILE_path
+sed -i 's/\([0-9][0-9][0-9]\)Z/\1/g' $FILE_path
+
+
+echo "remove bad lines from coastline"
+
+FILE_path2=data/coastline2.dat
+
+#>
+sed -i "/>/d" $FILE_path2
